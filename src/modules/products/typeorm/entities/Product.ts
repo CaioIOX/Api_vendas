daec905636dd8183fc52ac9c012f71af17c2b32f
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2022-12-14 11:01:01 -0300

    Criando o Entity product

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,2 +1,25 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
+@Entity('products')
+class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column('decimal')
+    price: number;
+
+    @Column('int')
+    quantity: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_At: Date;
+}
+
+export default Product;
