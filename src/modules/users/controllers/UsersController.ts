04782b94490df5f5301c89dd9a337b92abcfc1a3
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-01-05 09:30:51 -0300

    Criado as rotas e criptografia para a senha dos usuários criados

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import CreaterUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
 
-export default class UserController {
+export default class UsersController {
   public async index(req: Request, res: Response): Promise<Response> {
     const listUser = new ListUserService();
     const users = await listUser.execute();
