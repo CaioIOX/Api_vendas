a7a3df17e7830fec41425879fef72cea07d60105
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-01-05 10:14:19 -0300

    Implementado token de autenticação com JWT

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppError';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -9,12 +10,13 @@ interface IRequest {
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -25,9 +27,18 @@ class CreateSessionService {
     const passwordConfirmed = await compare(password, user.password);
 
     if (!passwordConfirmed) {
-      throw new AppError('Combinação de email e senha incorretos.', 401)
+      throw new AppError('Combinação de email e senha incorretos.', 401);
     }
-    return user;
+
+    // Criação do token de autenticação
+    const token = sign({}, '6a395dc1244891a95f5f4fe70e4a3dbf', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+    return {
+      user,
+      token,
+    };
   }
 }
 
