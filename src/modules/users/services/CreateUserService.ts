753c97e66b5f2596a52cd49fcb8d313f360b81da
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-01-05 08:46:10 -0300

    Criado serviço de criação de usuário

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreaterUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepository);
+    const emailExists = await userRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Já existe um usuário com este email.');
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreaterUserService;
