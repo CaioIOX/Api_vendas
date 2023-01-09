d79951c7baa82bd81a694165a9ffdc5b2f7116ff
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-01-09 11:08:34 -0300

    Criado controller do avatar de usuário

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -21,7 +21,7 @@ class UpdateUserAvatarService {
     }
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath); //Fs = FileSystem para manipulação de arquivos
 
       if (userAvatarFileExists) {
         await fs.promises.unlink(userAvatarFilePath);
