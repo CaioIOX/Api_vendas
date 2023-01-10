eda4f5ddf666e2a6b47fc3ac870b072f1ecf7c68
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-01-10 09:24:34 -0300

    Criado serviços de recuperação de senha

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import { isAfter, addHours } from 'date-fns';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokenRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('Token de usuário não existe.');
+    }
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado.');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expirado.');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,25 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokenRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!email) {
+      throw new AppError('Usuário não encontrado.');
+    }
+
+    const token = await userTokenRepository.generate(user?.id as string);
+
+    console.log(token);
+  }
+}
