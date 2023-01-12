f1a68ea45d5a47682c85d9a24cf48714549cfe65
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-01-12 11:50:52 -0300

    Criando Serviço de criação de pedidos part 1

---
+++ src/modules/orders/typeorm/services/CreateOrderService.ts
@@ -0,0 +1,36 @@
+import CustomersRepository from '@modules/customers/typeorm/repositories/CustomersRespository';
+import { ProductRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Order from '../entities/Orders';
+import OrdersRepository from '../repositories/OrdersRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+class CreateOrderService {
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const customerExists = await customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Cliente não encontrado.');
+    }
+
+    const productExists = await productsRepository.findAllByIds(products);
+
+    
+  }
+}
+
+export default CreateOrderService;
