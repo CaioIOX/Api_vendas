6858652e84fe3663dfdbe977ceb532b111e79d68
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-01-17 09:50:49 -0300

    Refatorando RedisCache

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,4 +1,4 @@
-import RedisCache from '@shared/cache/RedisCache';
+import redisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
@@ -15,8 +15,6 @@ class DeleteProductService {
     if (!product) {
       throw new AppError('Produto não encontrado.');
     }
-
-    const redisCache = new RedisCache();
 
     await redisCache.invalidate('api_vendas_PRODUCT_LIST');
 
